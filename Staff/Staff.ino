02065040d5efcd61fbe7ce9ca01ec8c5acc2fca2
Chad A. Woitas <woitas.chad@gmail.com>
2017-04-22 08:09:02 -0600

    added buttons and led debugging
    todo add functionaility to second button

--- Staff/Staff.ino
+++ Staff/Staff.ino
@@ -11,8 +11,10 @@
 
 CRGB leds[NUM_LEDS];
 
-//used to this style personally
-int buttonA = 13; //Adjust as needed
+//used to this style personally, I think in arduino int and define are similar
+int buttonA = 2; //Adjust as needed
+int buttonB = 3;
+int ledPin = 13; //debugging ----------------------
 int readPin = 0;
 CRGB PriorityColour;
 CRGB ColourRed;
@@ -22,11 +24,15 @@ CRGB ColourGreen;
 void setup() {
 
      delay(2000); //Why so high, I think this is unnessecary
+     
      FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
      ColourRed = CRGB(255,10,10);
      ColourGreen = CRGB(30,205,25);
      PriorityColour = ColourRed;
 
+     pinMode(buttonA, INPUT);
+     pinMode(buttonB, INPUT);
+     pinMode(ledPin,OUTPUT); //debugging --------------------------------
 } //End SetUp
 
 
@@ -43,7 +49,7 @@ void loop() {
   readPin = digitalRead(buttonA);
   
   if(readPin == HIGH){
-
+  digitalWrite(ledPin, HIGH); //debugging --------------------
     if(PriorityColour == ColourGreen){
       PriorityColour = ColourRed;
     }
@@ -56,6 +62,6 @@ void loop() {
   
   }//End Debounce 
   delay(1000);//This might be high,maybe 100
-
+  digitalWrite(ledPin,LOW);
 }//End Loop
 
